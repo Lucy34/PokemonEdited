1630c677a2d79a794ccdd2d109bc4b764890c998
Dabo Ross <dabodialga@gmail.com>
2013-03-03 11:17:23 -0800

    Fixed

--- src/net/daboross/engine/ImageHandler.java
+++ src/net/daboross/engine/ImageHandler.java
@@ -1,7 +1,6 @@
 package net.daboross.engine;
 
 import java.awt.image.BufferedImage;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
@@ -9,13 +8,11 @@ import net.daboross.will.pokemon.Pokemon;
 
 public final class ImageHandler {
 
-    public static BufferedImage getImage(String imgName) throws FileNotFoundException {
+    public static BufferedImage getImage(String imgName){
         String fullName = (imgName.startsWith("/") ? "/net/daboross/will/pokemon" : "/net/daboross/will/pokemon/") + imgName;
         BufferedImage img = null;
         URL fl = Pokemon.class.getResource(fullName);
-        if (fl == null) {
-            throw new FileNotFoundException(fullName);
-        } else {
+        if (fl != null) {
             try {
                 img = ImageIO.read(fl);
             } catch (IOException e) {
